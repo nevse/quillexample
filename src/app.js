ef56fb9f4433330f3fcd7b6fafb5747f193f7316
nevse <nov.eugen@gmail.com>
2023-08-29 18:34:50 +0400

    add

---
+++ src/app.js
@@ -0,0 +1,41 @@
+// import Quill from 'devextreme-quill';
+import "devextreme-quill/dist/dx-quill.min.js"
+import "devextreme-quill/dist/dx-quill.core.css";
+
+// import Bold from 'devextreme-quill/formats/bold';
+// import Italic from 'devextreme-quill/formats/italic';
+// import Header from 'devextreme-quill/formats/header';
+
+
+// Quill.register({
+//   'formats/bold': Bold,
+//   'formats/italic': Italic,
+//   'formats/header': Header
+// });
+
+// (function () {
+//   window["Quill"] = Quill;
+//   var quill = new Quill('#editor-container', {
+//     modules: {
+//       toolbar: [
+//         [{ header: [1, 2, 3, 4, 5, 6,  false] }],
+//         ['bold', 'italic', 'underline','strike'],
+//         ['image', 'code-block'],
+//         ['link'],
+//         [{ 'script': 'sub'}, { 'script': 'super' }],
+//         [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+//         ['clean']
+//       ]
+//     },
+//     placeholder: 'Compose an epic...',
+//     theme: 'snow'  // or 'bubble'
+//   });
+// })();
+
+
+(function () {
+   var quill = new Quill('#editor-container', {
+    
+   });
+
+ })();
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'web', // vscode extensions run in webworker context for VS Code web 📖 -> https://webpack.js.org/configuration/target/#target
+
+  entry: './src/app.js', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs2',
+  },
+  devtool: 'source-map',
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+  
+    extensions: ['.ts', '.js', '.css'],
+    alias: {
+      // provides alternate implementation for node module and source files
+    },
+    fallback: {
+      // Webpack 5 no longer polyfills Node.js core modules automatically.
+      // see https://webpack.js.org/configuration/resolve/#resolvefallback
+      // for the list of Node.js core module polyfills.
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+        
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        // {
+        //   test: /quill\.js$/,
+        //   use: [
+        //     'babel-loader'
+        //   ]
+        // },
+        {
+          test: /\.svg$/,
+          use: [{
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }]
+        }
+    ]
+  }
+};
+module.exports = config;
